e48da60eadfb12d64b1dd6080fcb5993c0a0a25d
Badal Prasad Singh <badalstar1806@gmail.com>
2023-11-15 19:16:54 +0530

    Starters 108

---
+++ Starters108/five.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+d = defaultdict(lambda: 0)
+
+for _ in range(int(input())):
+    n = int(input())
+    x = list(map(int, input().split()))
+    a, b, c = map(int, input().split())
+    s = input()
+
+
